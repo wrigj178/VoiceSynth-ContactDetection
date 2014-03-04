7acb761c58b57b6b4b2e076801ed3dbf320955e8
wrigj178 <jjazzymane@live.com>
2014-03-04 12:40:09 -0500

    Initial Commit

---
+++ VoiceSynth&ContactDectionTests/VoiceSynth_ContactDectionTests.m
@@ -0,0 +1,34 @@
+//
+//  VoiceSynth_ContactDectionTests.m
+//  VoiceSynth&ContactDectionTests
+//
+//  Created by Jasmine Wright on 3/4/14.
+//  Copyright (c) 2014 Jasmine Wright. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface VoiceSynth_ContactDectionTests : XCTestCase
+
+@end
+
+@implementation VoiceSynth_ContactDectionTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
